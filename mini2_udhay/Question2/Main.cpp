ac1b77daa3e29a84c4fb1eff8f5d2fe6bd4b0cae
LeelaKumari Gorupotu <leelakumari@xyz.com>
2024-02-16 10:08:42 +0530

    mini2_udhay

--- mini2_udhay/Question2/Main.cpp
+++ mini2_udhay/Question2/Main.cpp
@@ -21,57 +21,57 @@ int main()
 
     CreateObjects(data,dataMotor);
 
-    // try
-    // {
-    // std::variant<float,unsigned int> result = AcceptsIdReturnsTorqueOrPower(data,"123"); 
+    try
+    {
+    std::variant<float,unsigned int> result = AcceptsIdReturnsTorqueOrPower(data,"123");  //Function 1
     
-    //     std::visit([](auto&& val){std::cout<<"the Result of the Functions is "<<val<<std::endl;},result);
+        std::visit([](auto&& val){std::cout<<"the Result of the Functions is "<<val<<std::endl;},result);
         
-    // }
-    // catch(EmptyContainerException& emp)
-    // {
-    //     std::cout<<emp.what()<<std::endl;
-    // } 
-    // catch(ValidDataNotFoundException& val)
-    // {
-    //     std::cout<<val.what()<<std::endl;
-    // }  
+    }
+    catch(EmptyContainerException& emp)
+    {
+        std::cout<<emp.what()<<std::endl;
+    } 
+    catch(ValidDataNotFoundException& val)
+    {
+        std::cout<<val.what()<<std::endl;
+    }  
     
-//     try
-//     {
-//         DisplaysBatteryDrain(data);         //Function 2
-//     }
-//     catch(EmptyContainerException& emp)
-//     {
-//         std::cout<<emp.what()<<std::endl;
-//     }
+    try
+    {
+        DisplaysBatteryDrain(data);         //Function 2
+    }
+    catch(EmptyContainerException& emp)
+    {
+        std::cout<<emp.what()<<std::endl;
+    }
 
 
-//     try{                                    //Function 3
+    try{                                    //Function 3
 
-//     Container AllInstancesOfEvSharedPointer = FilterData(data,MaxPowerOver300);
+    Container AllInstancesOfEvSharedPointer = FilterData(data,MaxPowerOver300);
 
-//    for(ElectricVehiclePointer& evptr : AllInstancesOfEvSharedPointer)
-//    {
-//         std::cout<< *evptr<<std::endl;
-//    }
+   for(ElectricVehiclePointer& evptr : AllInstancesOfEvSharedPointer)
+   {
+        std::cout<< *evptr<<std::endl;
+   }
 
-//     }
-//     catch(EmptyContainerException& emp)
-//     {
-//         std::cout<<emp.what()<<std::endl;
-//     }
-//     catch(NoInstanceFoundException& err)
-//     {
-//         std::cout<<err.what()<<std::endl;
-//     }
+    }
+    catch(EmptyContainerException& emp)
+    {
+        std::cout<<emp.what()<<std::endl;
+    }
+    catch(NoInstanceFoundException& err)
+    {
+        std::cout<<err.what()<<std::endl;
+    }
         
 
                                     //Logic 5
 
-//    int CombinedMaxPower = (*data[0]).motor().get() + (*data[data.size()-1]).motor().get();
+   int CombinedMaxPower = (*data[0]).motor().get() + (*data[data.size()-1]).motor().get();
 
-//    std::cout<<"the Combined Power is "<<CombinedMaxPower<<std::endl;
+   std::cout<<"the Combined Power is "<<CombinedMaxPower<<std::endl;
 
 
     std::cout<<"Above Operation"<<std::endl;
